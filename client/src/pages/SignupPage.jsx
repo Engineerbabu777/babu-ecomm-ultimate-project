d2e92b347744d27394c5f7e855a4fc7de3901b44
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-16 06:57:42 +0500

    add: create user route

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Signup from "../components/Signup/Signup";
 
