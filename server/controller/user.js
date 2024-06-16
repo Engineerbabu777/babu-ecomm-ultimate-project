83801f5feceebdb4966c76618fd6229fdb7bb29e
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-16 06:58:12 +0500

    add: activate user route

--- server/controller/user.js
+++ server/controller/user.js
@@ -56,3 +56,47 @@ router.post("/create-user", async (req, res, next) => {
 });
 
 
+
+// create activation token
+const createActivationToken = (user) => {
+    return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+      expiresIn: "5m",
+    });
+  };
+  
+  // activate user
+  router.post(
+    "/activation",
+    catchAsyncErrors(async (req, res, next) => {
+      try {
+        const { activation_token } = req.body;
+  
+        const newUser = jwt.verify(
+          activation_token,
+          process.env.ACTIVATION_SECRET
+        );
+  
+        if (!newUser) {
+          return next(new ErrorHandler("Invalid token", 400));
+        }
+        const { name, email, password, avatar } = newUser;
+  
+        let user = await User.findOne({ email });
+  
+        if (user) {
+          return next(new ErrorHandler("User already exists", 400));
+        }
+        user = await User.create({
+          name,
+          email,
+          avatar,
+          password,
+        });
+  
+        sendToken(user, 201, res);
+      } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+    })
+  );
+  
