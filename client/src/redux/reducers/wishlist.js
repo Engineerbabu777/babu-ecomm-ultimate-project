98167418eee6e8d3a763635aa8b14ac5cb1fd25c
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-17 05:31:53 +0500

    Add: wishlist reducers

--- client/src/redux/reducers/wishlist.js
+++ client/src/redux/reducers/wishlist.js
@@ -1,21 +1,34 @@
-// add to wishlist
-export const addToWishlist = (data) => async (dispatch, getState) => {
-    dispatch({
-      type: "addToWishlist",
-      payload: data,
-    });
-  
-    localStorage.setItem("wishlistItems", JSON.stringify(getState().wishlist.wishlist));
-    return data;
-  };
-  
-  // remove from wishlist
-  export const removeFromWishlist = (data) => async (dispatch, getState) => {
-    dispatch({
-      type: "removeFromWishlist",
-      payload: data._id,
-    });
-    localStorage.setItem("wishlistItems", JSON.stringify(getState().wishlist.wishlist));
-    return data;
-  };
-  
+import { createReducer } from "@reduxjs/toolkit";
+
+const initialState = {
+  wishlist: localStorage.getItem("wishlistItems")
+    ? JSON.parse(localStorage.getItem("wishlistItems"))
+    : [],
+};
+
+export const wishlistReducer = createReducer(initialState, {
+  addToWishlist: (state, action) => {
+    const item = action.payload;
+    const isItemExist = state.wishlist.find((i) => i._id === item._id);
+    if (isItemExist) {
+      return {
+        ...state,
+        wishlist: state.wishlist.map((i) =>
+          i._id === isItemExist._id ? item : i
+        ),
+      };
+    } else {
+      return {
+        ...state,
+        wishlist: [...state.wishlist, item],
+      };
+    }
+  },
+
+  removeFromWishlist: (state, action) => {
+    return {
+      ...state,
+      wishlist: state.wishlist.filter((i) => i._id !== action.payload),
+    };
+  },
+});
